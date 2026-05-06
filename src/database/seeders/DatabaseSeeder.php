49769c334f43bb1e49f420449accd1b09bf1af0e
Theobald <f.moreau@theobald-groupe.com>
2026-05-06 10:15:48 +0200

    Add seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(WeatherModelSeeder::class);
-        
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
