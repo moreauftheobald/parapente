c43a948bfeab5b9012f790de74f5dcedb4db84bb
Theobald <f.moreau@theobald-groupe.com>
2026-05-16 11:43:08 +0200

    WIP on responsiv design. better map render.

--- src/resources/views/components/app-shell.blade.php
+++ src/resources/views/components/app-shell.blade.php
@@ -91,11 +91,9 @@
         aside { will-change: transform; }
     </style>
 
-    {{-- Font Awesome 6 (icônes des modules / actions) --}}
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer">
+    {{-- Font Awesome auto-hébergé via Vite (cf. resources/css/app.css) :
+         pas de dépendance CDN cloudflare qui était bloquée par
+         certains opérateurs mobiles / DNS publics. --}}
 
     @stack('styles')
 </head>
